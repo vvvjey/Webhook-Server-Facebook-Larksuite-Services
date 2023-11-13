28ceed1fd3be58bae194545a51fe23f1f5db09c3
vvvjey <hoangtu4520031234@gmail.com>
2023-11-13 23:33:23 +0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 var bodyParser = require('body-parser')
-
+const request = require('request');
 require('dotenv').config();
 app.use(cors())
 
@@ -30,16 +30,24 @@ app.post("/webhook", (req, res) => {
      // Gets the body of the webhook event
      body.entry.forEach(function(entry) {
 
-      let webhook_event = entry.messaging[0];
-      console.log('abc',webhook_event);
+            // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
 
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-        res.status(200).send("EVENT_RECEIVED");
 
-    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
      })
+
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
@@ -71,6 +79,19 @@ app.get("/webhook", (req, res) => {
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
 
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `Bạn đã gửi tin nhắn là: "${received_message.text}". H đi ngủ đi!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
 }
 
 // Handles messaging_postbacks events
@@ -80,7 +101,27 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }
 
 
